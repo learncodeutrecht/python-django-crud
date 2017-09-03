07ae81d25015df323609b1a4fe7b44594d02018d
RikSchoonbeek <rik.schoonbeek@gmail.com>
2017-09-03 18:34:36 +0200

    Added create 'Thought' function

--- pydjapp/pdcrud/views.py
+++ pydjapp/pdcrud/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+
+from .forms import ThoughtForm
+from .models import Thought
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'pdcrud/index.html')
+    if request.method == 'POST':
+        form = ThoughtForm(request.POST)
+        if form.is_valid():
+            # Have form saved as model instance in DB
+            thought = Thought(title=form.cleaned_data['title'],
+                              thought=form.cleaned_data['thought'])
+            thought.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = ThoughtForm()
+
+    return render(request, 'pdcrud/index.html', {'form': form})

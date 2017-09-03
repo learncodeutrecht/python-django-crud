de1814638b1a69d325e878535b02f9667dbce846
RikSchoonbeek <rik.schoonbeek@gmail.com>
2017-09-03 17:19:26 +0200

    Added thought model and made first ever migrations

--- pydjapp/pdcrud/models.py
+++ pydjapp/pdcrud/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Thought(models.Model):
+    title = models.TextField(max_length=60)
+    thought = models.TextField()
+
+
+    def __str__(self):
+        return self.title
+    

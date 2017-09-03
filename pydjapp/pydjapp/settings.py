4ef3d2f2f2011175e2ebf94df5a0dbaaaaaacb10
RikSchoonbeek <rik.schoonbeek@gmail.com>
2017-09-03 15:59:20 +0200

    Added template and template folder for pdcrud app

--- pydjapp/pydjapp/settings.py
+++ pydjapp/pydjapp/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'pydjapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

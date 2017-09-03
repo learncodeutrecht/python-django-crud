bb40f23f233731b399569f7e8c728ede42581964
RikSchoonbeek <rik.schoonbeek@gmail.com>
2017-09-03 18:34:36 +0200

    Added: create Thought

--- pydjapp/pdcrud/views.py
+++ pydjapp/pdcrud/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+#Import form and model for further use in view
+from .forms import ThoughtForm
+from .models import Thought
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'pdcrud/index.html')
+    # Either serve html page with empty form (GET) or create new table entry (POST)
+    if request.method == 'POST':
+        form = ThoughtForm(request.POST)  # Save POSTed form data as form
+        if form.is_valid():  # Check data validity
+            # Create new table entry from cleaned form data
+            thought = Thought(title=form.cleaned_data['title'],
+                              thought=form.cleaned_data['thought'])
+            thought.save()  # Save new table entry to table/DB
+            return HttpResponseRedirect('/')  # Redirect (GET) to domain root
+    else:
+        form = ThoughtForm()  # Form, to be served to template when rendering it
+
+    # Render template, serve it an empty form instance
+    return render(request, 'pdcrud/index.html', {'form': form})

d715c1927b4275cb11c3d894932d1ff2a7296289
Philippe <philippedev101@gmail.com>
2017-09-09 18:27:54 +0200

    First part of update page
    * Added update action
    * reused create form for updating, renamed to thoughtForm and added variables
    * added actions column to thought list
    * clean up views.py according to PEP rules
    * added venv (virtualenv) folder to .gitignore

--- pydjapp/pdcrud/urls.py
+++ pydjapp/pdcrud/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^index/$', views.create, name='create'),
     url(r'^create/$', views.create, name='create'),
+    url(r'^update/([0-9]+)/$', views.update, name='update'),
     url(r'^readall/$', views.readall, name='readall'),
 ]
--- pydjapp/pdcrud/views.py
+++ pydjapp/pdcrud/views.py
@@ -1,47 +1,53 @@
+"""views for the CRUD actions."""
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
-#Import form and model for further use in view
+# Import form and model for further use in view
 from .forms import ThoughtForm
 from .models import Thoughts
 
 
-# Create your views here.
 def index(request):
-    # Either serve html page with empty form (GET) or create new table entry (POST)
-    #if request.method == 'POST':
-    #    form = ThoughtForm(request.POST)  # Save POSTed form data as form
-    #    if form.is_valid():  # Check data validity
-    #        # Create new table entry from cleaned form data
-    #        thought = Thought(title=form.cleaned_data['title'],
-    #                          thought=form.cleaned_data['thought'])
-    #        thought.save()  # Save new table entry to table/DB
-    #        return HttpResponseRedirect('/')  # Redirect (GET) to domain root
-    #else:
-    #    form = ThoughtForm()  # Form, to be served to template when rendering it
-
+    """Create your views here."""
     # Render template, serve it an empty form instance
     return render(request, 'pdcrud/index.html')
 
-# GET and POST for the Create Form for creating THOUGHTS
+
 def create(request):
+    """GET and POST for the Create Form for creating THOUGHTS."""
     if request.method == 'POST':
         form = ThoughtForm(request.POST)  # Save POSTed form data as form
         if form.is_valid():  # Check data validity
             # Create new table entry from cleaned form data
             thought = Thoughts(thought=form.cleaned_data['thought'],
-                              thoughttype=form.cleaned_data['thoughttype'],
-                              author=form.cleaned_data['author'],
-                              date=form.cleaned_data['date'])
+                               thoughttype=form.cleaned_data['thoughttype'],
+                               author=form.cleaned_data['author'],
+                               date=form.cleaned_data['date'])
             thought.save()  # Save new table entry to table/DB
             return HttpResponseRedirect('/')  # Redirect (GET) to domain root
     else:
-        form = ThoughtForm()  # Form, to be served to template when rendering it
+        form = ThoughtForm()  # Form, to be served to template when rendering
 
-    return render(request, 'pdcrud/create.html', {'form': form})
+    return render(request,
+                  'pdcrud/thoughtForm.html',
+                  {'form': form,
+                   'title': 'Create a new thought.',
+                   'url': '/create/'})
 
-# REAL ALL existing thoughts and show them on the HTML page
+
+def update(request, id):
+    """Update a single record."""
+    form = ThoughtForm()
+
+    return render(request,
+                  'pdcrud/thoughtForm.html',
+                  {'form': form,
+                   'title': 'Update thought.',
+                   'url': '/update/' + str(id)})
+
+
 def readall(request):
+    """REAL ALL existing thoughts and show them on the HTML page."""
     thoughts = Thoughts.objects.all()
-    #thoughts = []
-    return render(request, 'pdcrud/thoughts.html', {'thoughts':thoughts})
+    # thoughts = []
+    return render(request, 'pdcrud/thoughts.html', {'thoughts': thoughts})
